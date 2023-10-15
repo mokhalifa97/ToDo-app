ab13b25c4ed6c11c4b9b420393608fdf1faff30d
mohamed ezzat <mohamad.5halifa1997@gmail.com>
2023-10-15 21:31:04 +0300

    add status column to tasks

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,7 @@
 
         <div class="col-md-6">
             <div class="card">
-                <div class="card-header">welcome {{$name}}</div>
+                <div class="card-header"><h5>{{$name}}</h5></div>
 
                 <div class="card-body">
                     @if (session('cate'))
@@ -22,6 +22,7 @@
                             <th scope="col">ID</th>
                             <th scope="col">User_id</th>
                             <th scope="col">Tasks</th>
+                            <th scope="col">Status</th>
                             <th scope="col">Operation</th>
                             </tr>
                         </thead>
@@ -31,9 +32,25 @@
                             <td>{{$item->id}}</td>
                             <td>{{$item->user_id}}</td>
                             <td>{{$item->task}}</td>
+                            <td>
+                                @if ($item->status=="not complete")
+                                <div class="badge bg-warning">
+                                    Not Complete
+                                </td>
+                                </div>
+                                @else
+                                <div class="badge bg-success">
+                                    Complete
+                                </td>
+                                </div>
+                                @endif
+                                
                             <td class="d-flex">
                                 <a href="{{route('delete',$item->id)}}" class="btn btn-danger ml-1">
                                     <i class="fa-solid fa-trash"></i>
+                                </a>
+                                <a href="{{route('delete',$item->id)}}" class="btn btn-info ml-1">
+                                    Edite
                                 </a>
                             </td>
                             </tr>
--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -3,9 +3,10 @@
 @section('content')
                 
     <section class="vh-100 gradient-custom">
-        <div class="container py-5 h-100">
+        <div class="container  h-100">
         <div class="row d-flex justify-content-center align-items-center h-100">
             <div class="col col-xl-10">
+                <h1>Create New Tasks</h1>
     
             <div class="card">
                 <div class="card-body p-5">
@@ -18,27 +19,10 @@
                         
                         <input type="text" id="form2" class="form-control" placeholder="New task..." aria-label="Recipient's username" aria-describedby="button-addon2" name="newDo">
                         
-                        <button class="btn btn-info" type="submit" id="button-addon2" name="addDo">Button</button>
+                        <button class="btn btn-success" type="submit" id="button-addon2" name="addDo">ADD</button>
                     </div>
                 </form>
-    
-                <!-- Tabs navs -->
-                <ul class="nav nav-tabs mb-4 pb-2" id="ex1" role="tablist">
-                    <li class="nav-item" role="presentation">
-                    <a class="nav-link active" id="ex1-tab-1" data-mdb-toggle="tab" href="#ex1-tabs-1" role="tab"
-                        aria-controls="ex1-tabs-1" aria-selected="true">All</a>
-                    </li>
-                    <li class="nav-item" role="presentation">
-                    <a class="nav-link" id="ex1-tab-2" data-mdb-toggle="tab" href="#ex1-tabs-2" role="tab"
-                        aria-controls="ex1-tabs-2" aria-selected="false">Active</a>
-                    </li>
-                    <li class="nav-item" role="presentation">
-                    <a class="nav-link" id="ex1-tab-3" data-mdb-toggle="tab" href="#ex1-tabs-3" role="tab"
-                        aria-controls="ex1-tabs-3" aria-selected="false">Completed</a>
-                    </li>
-                </ul>
-                <!-- Tabs navs -->
-    
+
                 <!-- Tabs content -->
                 <div class="tab-content" id="ex1-content">
                     <div class="tab-pane fade show active" id="ex1-tabs-1" role="tabpanel"
@@ -49,13 +33,19 @@
                         @if (empty($task))
                         <div class="alert alert-danger text-center">No Added Tasks Yet</div>
                         @else
+
                         @foreach ($task as $item)
                         <ul class="list-group mb-0">
-                                <li class="list-group-item d-flex align-items-center border-0 mb-2 rounded"
-                                    style="background-color: #f4f6f7;">
-                                <input class="form-check-input me-2" type="checkbox" value="" aria-label="..." checked />
-                            {{-- <s>Cras justo odio</s> --}}
-                            {{$item->task}}
+                            <li class="list-group-item d-flex align-items-center border-0 mb-2 rounded"
+                                style="background-color: #f4f6f7;">
+
+                        @if ($item->status=="not complete")
+                                <input class="form-check-input me-2" type="checkbox" value="" aria-label="..." />
+                                {{$item->task}}
+                        @else
+                        <input class="form-check-input me-2" type="checkbox" value="" aria-label="..." checked/>
+                        <s>{{$item->task}}</s>
+                        @endif
                             </li> 
                         @endforeach
                         @endif
