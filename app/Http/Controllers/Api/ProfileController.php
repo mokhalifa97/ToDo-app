c83cb079a859ab4c0cdbdd20106478947ccd47e7
mohamed ezzat <mohamad.5halifa1997@gmail.com>
2023-10-16 22:04:50 +0300

    finish all api's

--- app/Http/Controllers/Api/ProfileController.php
+++ app/Http/Controllers/Api/ProfileController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\ProfileResource;
 use App\Models\Profile;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class ProfileController extends Controller
 {
@@ -15,6 +16,100 @@ class ProfileController extends Controller
             'msg' => 'All profile table records',
             'status' => 200,
             'data' => $tasks
+        ];
+        return response()->json($data);
+    }
+
+
+    public function show($id){
+        $task= Profile::find($id);
+        if($task){
+            $data=[
+                'msg' => 'one record',
+                'status' => 200,
+                'data' => new ProfileResource($task)
+            ];
+            return response()->json($data);
+        }else{
+            $data=[
+                'msg' => 'there is no such record!!',
+                'status' => 201,
+                'data' => null
+            ];
+            return response()->json($data);
+        }
+    }
+
+    public function delete(Request $request){
+        $id= $request->id;
+        $task = Profile::find($id);
+        $task->delete();
+        $data=[
+            "msg"=> "deleted successfully !",
+            "status"=> 205,
+            "data" => null
+        ];
+        return response()->json($data);
+    }
+
+    public function update(Request $request){
+        $validateData= Validator::make($request->all(),[
+            'id' => 'required|unique:profiles|max:11',
+            'task' => 'required',
+            'status' => 'required',
+        ]);
+
+        if($validateData->fails()){
+            $data=[
+                "msg"=> "NOT VALID DATA!!",
+                "status"=> 203,
+                "data" => $validateData->errors()
+            ];
+            return response()->json($data);
+        }
+
+        $old_id= $request->old;
+        $task= Profile::find($old_id);
+        $task->update([
+            'id' => $request->id,
+            'task' => $request->task,
+            'status' => $request->status,
+        ]);
+        $data=[
+            "msg"=> "updated record successfully",
+            "status"=> 207,
+            "data" => new ProfileResource($task)
+        ];
+        return response()->json($data);
+    }
+
+    public function create(Request $request){
+        $validateData= Validator::make($request->all(),[
+            'id' => 'unique:profiles|max:11',
+            'task' => 'required',
+            'status' => 'required',
+        ]);
+
+        if($validateData->fails()){
+            $data=[
+                "msg"=> "NOT VALID DATA!!",
+                "status"=> 203,
+                "data" => $validateData->errors()
+            ];
+            return response()->json($data);
+        }
+
+        $newTask= Profile::create([
+            'id' => $request->id,
+            'task' => $request->task,
+            'status' => $request->status,
+            'user_id' => $request->user_id,
+        ]);
+
+        $data=[
+            "msg"=> "CREATED NEW RECORD SUCCESSFULLY!!",
+            "status"=> 201,
+            "data" => new ProfileResource($newTask)
         ];
         return response()->json($data);
     }
--- routes/api.php
+++ routes/api.php
@@ -22,4 +22,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::prefix('v1')->group(function(){
     Route::get('/profile',[ApiProfileController::class,'index']);
+    Route::get('/show/{id}',[ApiProfileController::class,'show']);
+    Route::post('/delete',[ApiProfileController::class,'delete']);
+    Route::post('/update',[ApiProfileController::class,'update']);
+    Route::post('/create',[ApiProfileController::class,'create']);
 });
